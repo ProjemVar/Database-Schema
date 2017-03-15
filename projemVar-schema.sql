62fe1f8576d5622d81f3ac3f0c22f8d4b7c489cd
pleycpl <h4rvey@protonmail.com>
2017-03-15 17:51:04 +0300

    Projects tablosu eklendi

--- projemVar-schema.sql
+++ projemVar-schema.sql
@@ -5,21 +5,35 @@
 -- email_public degiskenide sitedeki herkesin ulasabilecegi email adresi yapildi.
 DROP TABLE Users CASCADE;
 CREATE TABLE Users (
-    nickname varchar(12) not null,
-    email_private varchar(20) not null,
-    password varchar(20) not null,
-    email_public varchar(20),
+    nickname        varchar(12) not null,
+    email_private   varchar(20) not null,
+    password        varchar(20) not null,
+    email_public    varchar(20),
     primary key (nickname)
 );
 
 -- UsersSocialMedia tablosuda Users larin sosyal medya hesaplari tutuyor.
 DROP TABLE UsersSocialMedia CASCADE;
 CREATE TABLE UsersSocialMedia (
-    nickname varchar(12) not null,
-    website varchar(20),
-    github varchar(20),
-    stackoverflow varchar(20),
-    medium varchar(20),
+    nickname        varchar(12) not null,
+    website         varchar(20),
+    github          varchar(20),
+    stackoverflow   varchar(20),
+    medium          varchar(20),
     primary key (nickname),
     foreign key (nickname) references Users(nickname)
+);
+
+DROP TABLE Projects CASCADE;
+CREATE TABLE Projects (
+    project_name        varchar(20) not null,
+    creator_nickname    varchar(20) not null,
+    createdDate         date,
+    project_size        int,    -- quota 5 or 10 or limitless
+    project_status      int,    -- 1 ? Active : 2 ? Maintain : 3 ? Finished : None
+    project_content     varchar(100) not null,
+    project_team        varchar(20) not null, -- memberships
+    project_tags        varchar(20) not null, -- tags like js, nodejs, express, python
+    primary key (creator_nickname, project_name),
+    foreign key (creator_nickname) references Users(nickname)
 );
