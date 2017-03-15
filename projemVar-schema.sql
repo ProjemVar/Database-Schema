19cb5f8a5a85a519addf02df2847c28ef76f0026
pleycpl <h4rvey@protonmail.com>
2017-03-15 17:39:51 +0300

    added UsersSocialMedia Table

--- projemVar-schema.sql
+++ projemVar-schema.sql
@@ -1,4 +1,9 @@
-DROP_TABLE Users CASCADE;
+-- LastTest = 15.03.17
+-- Suanlik PostgreSql de denedim! Calisiyor;
+
+-- Users tablosunda kullanici girisi icin 'nickname' ve 'email_private' degiskenleri yapildi.
+-- email_public degiskenide sitedeki herkesin ulasabilecegi email adresi yapildi.
+DROP TABLE Users CASCADE;
 CREATE TABLE Users (
     nickname varchar(12) not null,
     email_private varchar(20) not null,
@@ -7,3 +12,14 @@ CREATE TABLE Users (
     primary key (nickname)
 );
 
+-- UsersSocialMedia tablosuda Users larin sosyal medya hesaplari tutuyor.
+DROP TABLE UsersSocialMedia CASCADE;
+CREATE TABLE UsersSocialMedia (
+    nickname varchar(12) not null,
+    website varchar(20),
+    github varchar(20),
+    stackoverflow varchar(20),
+    medium varchar(20),
+    primary key (nickname),
+    foreign key (nickname) references Users(nickname)
+);
