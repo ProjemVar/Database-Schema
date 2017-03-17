e62d8ef3c9e1f59dae27c2ae35664cd199de4355
pleycpl <h4rvey@protonmail.com>
2017-03-17 12:18:53 +0300

    mysql denendi calisiyor.

---
+++ projemVar-schema-maria.sql
@@ -0,0 +1,63 @@
+-- LastTest = 15.03.17
+-- Suanlik Mysql de denedim! Calisiyor;
+
+-- Users tablosunda kullanici girisi icin 'nickname' ve 'email_private' degiskenleri yapildi.
+-- email_public degiskenide sitedeki herkesin ulasabilecegi email adresi yapildi.
+SET FOREIGN_KEY_CHECKS=0;   -- to disable them
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users (
+    nickname        varchar(12) not null,
+    email_private   varchar(20) not null,
+    password        varchar(20) not null,
+    email_public    varchar(20),
+    unique(nickname, email_private),
+    primary key (nickname)
+);
+
+-- UsersSocialMedia tablosuda Users larin sosyal medya hesaplari tutuyor.
+DROP TABLE IF EXISTS UsersSocialMedia;
+CREATE TABLE UsersSocialMedia (
+    nickname        varchar(12) not null,
+    website         varchar(20),
+    github          varchar(20),
+    stackoverflow   varchar(20),
+    medium          varchar(20),
+    unique(nickname),
+    primary key (nickname),
+    foreign key (nickname) references Users(nickname)
+);
+
+-- Projects tablosunda projelerin ozelliklerini tutuyoruz.
+DROP TABLE IF EXISTS Projects;
+CREATE TABLE Projects (
+    pno			        int,
+    project_name        varchar(20) not null,
+    creator_nickname    varchar(20) not null,
+    createdDate         date,
+    project_size        int,    -- quota 5 or 10 or limitless
+    project_status      int,    -- 1 ? Active : 2 ? Maintain : 3 ? Finished : None
+    project_content     varchar(100) not null,
+    unique(project_name),
+    primary key (pno),
+    foreign key (creator_nickname) references Users(nickname)
+);
+
+
+-- ProjectsTeam tablosunda projede bulunan uyeler var.
+DROP TABLE IF EXISTS ProjectsTeam;
+CREATE TABLE ProjectsTeam ( -- membership
+    pno	        int,
+    nickname 	varchar(20) not null,
+    primary key(pno, nickname),
+    foreign key(pno) references Projects(pno),
+    foreign key(nickname) references Users(nickname)
+);
+
+-- ProjectsTags tablosunda projenin tag leri yani 'nodejs', 'cava', 'fayton' gibi ifadeleri tutuyoruz.
+DROP TABLE IF EXISTS ProjectsTags;
+CREATE TABLE ProjectsTags (
+    pno	int,
+    tag	varchar(15) not null,
+    primary key(pno),
+    foreign key(pno) references Projects(pno)
+);
